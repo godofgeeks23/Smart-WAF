6f9d9756b8a66c7e24cdb0e3dce75f826f6439e6
Aviral Srivastava <aviralji4@gmail.com>
2023-04-05 09:37:14 +0530

    Update burpparser.py

--- burpparser.py
+++ burpparser.py
@@ -63,7 +63,7 @@ def parse_burp_logs(log_file, output_file):
         with open(output_file, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(['Time', 'URL', 'IP Address', 'Port', 'Method',
-                            'Path', 'Extension', 'Request', 'Status', 'Response', 'method', 'url', 'version', 'headers', 'body'])
+                            'Path', 'Extension', 'Request', 'Status', 'Response', 'method', 'url', 'version', 'headers', 'body', 'count_single_quote','count_double_quote','count_backslash','count_backtick','count_dash','count_round_brackets','count_curly_brackets','count_sq_brackets','count_ang_brackets','count_forwardslash','count_equals','count_bad_words'])
             for item in items:
 
                 time = item.getElementsByTagName(
@@ -90,11 +90,39 @@ def parse_burp_logs(log_file, output_file):
                 request_decoded = request_decoded.decode('utf-8')
                 response_decoded = base64.b64decode(response)
                 response_decoded = response_decoded.decode('utf-8')
+
                 request_dict = parse_request_gpt(request_decoded)
+                response_dict = parse_request_gpt(response_decoded)
+                # print(response_dict)
                 
+                # calculate single quote, double quote, backslash, backtick, dash, brackets and other characters used in attacks like SQLi, XSS, etc.
+                count_single_quote = request_decoded.count("'")
+                count_double_quote = request_decoded.count('"')
+                count_backslash = request_decoded.count('\\')
+                count_backtick = request_decoded.count('`')
+                count_dash = request_decoded.count('--')
+                count_round_brackets = request_decoded.count('(')
+                count_round_brackets += request_decoded.count(')')
+                count_curly_brackets = request_decoded.count('{')
+                count_curly_brackets += request_decoded.count('}')
+                count_sq_brackets = request_decoded.count('[')
+                count_sq_brackets += request_decoded.count(']')
+                count_ang_brackets = request_decoded.count('<')
+                count_ang_brackets += request_decoded.count('>')
+                count_forwardslash = request_decoded.count('/')
+                count_equals = request_decoded.count('=')
+
+                # form a list of common words used in attacks like SQLi, XSS, etc.
+                bad_words = ['select', 'insert', 'update', 'delete', 'drop', 'alter', 'create', 'table', 'from', 'where', 'and', 'or', 'union', 'join', 'having', 'group', 'by', 'order', 'limit', 'offset', 'into', 'values', 'set', 'like', 'script', 'alert', 'onload', 'onerror', 'onmouseover', 'onmouseout', 'onfocus', 'onblur', 'onchange', 'onsubmit', 'onreset', 'onselect', 'onunload', 'onkeydown', 'onkeypress', 'onkeyup', 'img', 'src', 'href', 'url']
+
+                # count the number of bad words in the request (case insensitive)
+                count_bad_words = 0
+                for word in bad_words:
+                    count_bad_words += request_decoded.lower().count(word)
+
                 writer.writerow([time, url, ip_address, port, method,
-                                path, extension, request, status, response, request_dict['method'], request_dict['url'], request_dict['version'], request_dict['headers'], request_dict['body']])
+                                path, extension, request, status, response, request_dict['method'], request_dict['url'], request_dict['version'], request_dict['headers'], request_dict['body'], count_single_quote, count_double_quote, count_backslash, count_backtick, count_dash, count_round_brackets, count_curly_brackets, count_sq_brackets, count_ang_brackets, count_forwardslash, count_equals, count_bad_words])
 
 
-parse_burp_logs("testfire_burp.log", "testfire_burp.csv")
+parse_burp_logs("normalTrafficTraining.txt", "trainingdata_csic.csv")
 
